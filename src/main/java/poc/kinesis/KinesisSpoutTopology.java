89360306792edcec75b742277b5bfc2406511d32
Tao Xia <tao@udacity.com>
2017-10-13 16:19:02 -0700

    add local mode

--- src/main/java/poc/kinesis/KinesisSpoutTopology.java
+++ src/main/java/poc/kinesis/KinesisSpoutTopology.java
@@ -21,17 +21,24 @@ import com.amazonaws.ClientConfiguration;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.kinesis.model.ShardIteratorType;
 import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
 import org.apache.storm.StormSubmitter;
 import org.apache.storm.generated.AlreadyAliveException;
 import org.apache.storm.generated.AuthorizationException;
 import org.apache.storm.generated.InvalidTopologyException;
 import org.apache.storm.kinesis.spout.*;
 import org.apache.storm.topology.TopologyBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 
 public class KinesisSpoutTopology {
+    private static final Logger LOG = LoggerFactory.getLogger(SampleTopology.class);
+
     public static void main (String args[]) throws InvalidTopologyException, AuthorizationException, AlreadyAliveException {
+        String mode = null;
+        mode = args[0];
         RecordToTupleMapper recordToTupleMapper = new TestRecordToTupleMapper();
         KinesisConnectionInfo kinesisConnectionInfo = new KinesisConnectionInfo(new CredentialsProviderChain(), new ClientConfiguration(), Regions.US_WEST_2,
                 1000);
@@ -45,6 +52,14 @@ public class KinesisSpoutTopology {
         Config topologyConfig = new Config();
         topologyConfig.setDebug(true);
         topologyConfig.setNumWorkers(3);
-        StormSubmitter.submitTopology("SampleTopology", topologyConfig, topologyBuilder.createTopology());
+
+
+        if (mode.equals("LocalMode")) {
+            LOG.info("Starting sample storm topology in LocalMode ...");
+            new LocalCluster().submitTopology("SampleTopology", topologyConfig, topologyBuilder.createTopology());
+        }else{
+            LOG.info("Starting sample storm topology in RemoteMode ...");
+            StormSubmitter.submitTopology("SampleTopology", topologyConfig, topologyBuilder.createTopology());
+        }
     }
 }
